592937227471c18cc481b942d6016f8aec8d493c
tangjia <scarletttang89@163.com>
2017-03-28 14:09:29 +0800

    install devise

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user! , only: [:new]
   def index
     @groups = Group.all
   end
@@ -24,7 +25,7 @@ class GroupsController < ApplicationController
     end
   end
 
-  
+
 
   def update
     @group = Group.find(params[:id])
